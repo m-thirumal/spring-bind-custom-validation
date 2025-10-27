c7941da1e9966ee57b9e03c28ea5796759cc251c
Thirumal <m.thirumal@hotmail.com>
2025-10-27 07:00:52 +0530

    first commit

---
+++ src/main/java/com/thirumal/SpringBindCustomValidationApplication.java
@@ -0,0 +1,13 @@
+package com.thirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBindCustomValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBindCustomValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thirumal/SpringBindCustomValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thirumal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBindCustomValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
